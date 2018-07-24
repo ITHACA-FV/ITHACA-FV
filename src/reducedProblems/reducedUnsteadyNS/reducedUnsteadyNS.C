0d8eefeed1469bc7f39f20d24a33ab4535f02f71
mgadalla <gadalla.mah@gmail.com>
2018-07-24 21:14:47 +0200

    Improve reducedUnsteadyNS classes to avoid objects copying

--- src/reducedProblems/reducedUnsteadyNS/reducedUnsteadyNS.C
+++ src/reducedProblems/reducedUnsteadyNS/reducedUnsteadyNS.C
@@ -37,75 +37,52 @@ License
 
 // * * * * * * * * * * * * * * * Constructors * * * * * * * * * * * * * * * * //
 
-// Constructor
+// Constructor initialization
 reducedUnsteadyNS::reducedUnsteadyNS()
 {
-
 }
 
-reducedUnsteadyNS::reducedUnsteadyNS(unsteadyNS& problem, word tipo)
+reducedUnsteadyNS::reducedUnsteadyNS(unsteadyNS& FOMproblem)
 {
-    B_matrix = problem.B_matrix;
-    C_matrix = problem.C_matrix;
-    M_matrix = problem.M_matrix;
-    K_matrix = problem.K_matrix;
-    N_BC = problem.inletIndex.rows();
+    problem = &FOMproblem;
 
-    Nphi_u = B_matrix.rows();
-    Nphi_p = K_matrix.cols();
-
-    // Pressure Poisson equation approach
-    if (tipo == "PPE")
-    {
-        D_matrix = problem.D_matrix;
-        G_matrix = problem.G_matrix;
-        BC1_matrix = problem.BC1_matrix;
-        BC2_matrix = problem.BC2_matrix;
-        BC3_matrix = problem.BC3_matrix;
-        newton_object_PPE = newton_unsteadyNS_PPE(Nphi_u + Nphi_p , Nphi_u + Nphi_p, problem);
-        newton_object_sup.nu = nu;
-    }
-
-    // Supremizer approach
-    if (tipo == "SUP")
-    {
-        P_matrix = problem.P_matrix;
-        newton_object_sup = newton_unsteadyNS_sup(Nphi_u + Nphi_p , Nphi_u + Nphi_p, problem);
-        newton_object_sup.nu = nu;
-    }
+    N_BC = problem->inletIndex.rows();
+    Nphi_u = problem->B_matrix.rows();
+    Nphi_p = problem->K_matrix.cols();
 
     // Create locally the velocity modes
-    for (label k = 0; k < problem.liftfield.size(); k++)
+    for (label k = 0; k < problem->liftfield.size(); k++)
     {
-        Umodes.append(problem.liftfield[k]);
+        Umodes.append(problem->liftfield[k]);
     }
-    for (label k = 0; k < problem.NUmodes; k++)
+    for (label k = 0; k < problem->NUmodes; k++)
     {
-        Umodes.append(problem.Umodes[k]);
+        Umodes.append(problem->Umodes[k]);
     }
-    for (label k = 0; k < problem.NSUPmodes; k++)
+    for (label k = 0; k < problem->NSUPmodes; k++)
     {
-        Umodes.append(problem.supmodes[k]);
+        Umodes.append(problem->supmodes[k]);
     }
 
     // Create locally the pressure modes
-    for (label k = 0; k < problem.NPmodes; k++)
+    for (label k = 0; k < problem->NPmodes; k++)
     {
-        Pmodes.append(problem.Pmodes[k]);
+        Pmodes.append(problem->Pmodes[k]);
     }
 
     // Store locally the snapshots for projections
-    for (label k = 0; k < problem.Ufield.size(); k++)
+    for (label k = 0; k < problem->Ufield.size(); k++)
     {
-        Usnapshots.append(problem.Ufield[k]);
-        Psnapshots.append(problem.Pfield[k]);
+        Usnapshots.append(problem->Ufield[k]);
+        Psnapshots.append(problem->Pfield[k]);
     }
+    newton_object_sup = newton_unsteadyNS_sup(Nphi_u + Nphi_p , Nphi_u + Nphi_p, FOMproblem);
+    newton_object_PPE = newton_unsteadyNS_PPE(Nphi_u + Nphi_p , Nphi_u + Nphi_p, FOMproblem);
 }
 
+// * * * * * * * * * * * * * Operators supremizer  * * * * * * * * * * * * * //
 
-// * * * * * * * * * * * * * * * Operators supremizer  * * * * * * * * * * * * * //
-
-// Operator to evaluate the residual for the supremizer approach
+// Operator to evaluate the residual for the Supremizer approach
 int newton_unsteadyNS_sup::operator()(const Eigen::VectorXd &x, Eigen::VectorXd &fvec) const
 {
     Eigen::VectorXd a_dot(Nphi_u);
@@ -118,17 +95,17 @@ int newton_unsteadyNS_sup::operator()(const Eigen::VectorXd &x, Eigen::VectorXd 
     // Convective term
     Eigen::MatrixXd cc(1, 1);
     // Mom Term
-    Eigen::VectorXd M1 = B_matrix * a_tmp * nu;
+    Eigen::VectorXd M1 = problem->B_matrix * a_tmp * nu;
     // Gradient of pressure
-    Eigen::VectorXd M2 = K_matrix * b_tmp;
+    Eigen::VectorXd M2 = problem->K_matrix * b_tmp;
     // Mass Term
-    Eigen::VectorXd M5 = M_matrix * a_dot;
+    Eigen::VectorXd M5 = problem->M_matrix * a_dot;
     // Pressure Term
-    Eigen::VectorXd M3 = P_matrix * a_tmp;
+    Eigen::VectorXd M3 = problem->P_matrix * a_tmp;
 
     for (label i = 0; i < Nphi_u; i++)
     {
-        cc = a_tmp.transpose() * C_matrix[i] * a_tmp;
+        cc = a_tmp.transpose() * problem->C_matrix[i] * a_tmp;
         fvec(i) = - M5(i) + M1(i) - cc(0, 0) - M2(i);
     }
     for (label j = 0; j < Nphi_p; j++)
@@ -148,13 +125,12 @@ int newton_unsteadyNS_sup::df(const Eigen::VectorXd &x,  Eigen::MatrixXd &fjac) 
 {
     Eigen::NumericalDiff<newton_unsteadyNS_sup> numDiff(*this);
     numDiff.df(x, fjac);
-
     return 0;
 }
 
 // * * * * * * * * * * * * * * * Operators PPE * * * * * * * * * * * * * * * //
 
-// Operator to evaluate the residual for the supremizer approach
+// Operator to evaluate the residual for the Pressure Poisson Equation (PPE) approach
 int newton_unsteadyNS_PPE::operator()(const Eigen::VectorXd &x, Eigen::VectorXd &fvec) const
 {
     Eigen::VectorXd a_dot(Nphi_u);
@@ -169,32 +145,30 @@ int newton_unsteadyNS_PPE::operator()(const Eigen::VectorXd &x, Eigen::VectorXd 
     Eigen::MatrixXd gg(1, 1);
     Eigen::MatrixXd bb(1, 1);
     // Mom Term
-    Eigen::VectorXd M1 = B_matrix * a_tmp * nu;
+    Eigen::VectorXd M1 = problem->B_matrix * a_tmp * nu;
     // Gradient of pressure
-    Eigen::VectorXd M2 = K_matrix * b_tmp;
+    Eigen::VectorXd M2 = problem->K_matrix * b_tmp;
     // Mass Term
-    Eigen::VectorXd M5 = M_matrix * a_dot;
+    Eigen::VectorXd M5 = problem->M_matrix * a_dot;
     // Pressure Term
-    Eigen::VectorXd M3 = D_matrix * b_tmp;
+    Eigen::VectorXd M3 = problem->D_matrix * b_tmp;
 
     // BC PPE
-    Eigen::VectorXd M6 = BC1_matrix * a_tmp * nu;
+    Eigen::VectorXd M6 = problem->BC1_matrix * a_tmp * nu;
 
     // BC PPE
-    Eigen::VectorXd M7 = BC3_matrix * a_tmp * nu;
-
-
+    Eigen::VectorXd M7 = problem->BC3_matrix * a_tmp * nu;
 
     for (label i = 0; i < Nphi_u; i++)
     {
-        cc = a_tmp.transpose() * C_matrix[i] * a_tmp;
+        cc = a_tmp.transpose() * problem->C_matrix[i] * a_tmp;
         fvec(i) = - M5(i) + M1(i) - cc(0, 0) - M2(i);
     }
     for (label j = 0; j < Nphi_p; j++)
     {
         label k = j + Nphi_u;
-        gg = a_tmp.transpose() * G_matrix[j] * a_tmp;
-        bb = a_tmp.transpose() * BC2_matrix[j] * a_tmp;
+        gg = a_tmp.transpose() * problem->G_matrix[j] * a_tmp;
+        bb = a_tmp.transpose() * problem->BC2_matrix[j] * a_tmp;
         //fvec(k) = M3(j, 0) - gg(0, 0) - M6(j, 0) + bb(0, 0);
         fvec(k) = M3(j, 0) + gg(0, 0) - M7(j, 0);
     }
@@ -214,22 +188,18 @@ int newton_unsteadyNS_PPE::df(const Eigen::VectorXd &x,  Eigen::MatrixXd &fjac) 
 }
 
 
+// * * * * * * * * * * * * * Solve Functions supremizer * * * * * * * * * * * //
 
-// * * * * * * * * * * * * * * * Solve Functions  * * * * * * * * * * * * * //
 void reducedUnsteadyNS::solveOnline_sup(Eigen::MatrixXd& vel_now, label startSnap)
 {
     // Create and resize the solution vector
     y.resize(Nphi_u + Nphi_p, 1);
     y.setZero();
 
-    // Set Initial Conditions
-    // if(this->tstart!=0)
-    // {
+    // y.head(Nphi_u) = ITHACAutilities::get_coeffs(problem->Ufield[startSnap], Umodes);
+    // y.tail(Nphi_p) = ITHACAutilities::get_coeffs(problem->Pfield[startSnap], Pmodes);
     y.head(Nphi_u) = ITHACAutilities::get_coeffs(Usnapshots[startSnap], Umodes);
     y.tail(Nphi_p) = ITHACAutilities::get_coeffs(Psnapshots[startSnap], Pmodes);
-    // }
-
-
 
     // Change initial condition for the lifting function
     for (label j = 0; j < N_BC; j++)
@@ -237,13 +207,12 @@ void reducedUnsteadyNS::solveOnline_sup(Eigen::MatrixXd& vel_now, label startSna
         y(j) = vel_now(j, 0);
     }
 
-
     // Set some properties of the newton object
     newton_object_sup.nu = nu;
     newton_object_sup.y_old = y;
     newton_object_sup.dt = dt;
-    newton_object_sup.B_matrix = B_matrix;
     newton_object_sup.BC.resize(N_BC);
+
     for (label j = 0; j < N_BC; j++)
     {
         newton_object_sup.BC(j) = vel_now(j, 0);
@@ -260,7 +229,7 @@ void reducedUnsteadyNS::solveOnline_sup(Eigen::MatrixXd& vel_now, label startSna
     // Counting variable
     int counter = 0;
 
-    // Create vectpr to store temporal solution and save initial condition as first solution
+    // Create vector to store temporal solution and save initial condition as first solution
     Eigen::MatrixXd tmp_sol(Nphi_u + Nphi_p + 1, 1);
     tmp_sol(0) = time;
     tmp_sol.col(0).tail(y.rows()) = y;
@@ -275,7 +244,6 @@ void reducedUnsteadyNS::solveOnline_sup(Eigen::MatrixXd& vel_now, label startSna
     Color::Modifier green(Color::FG_GREEN);
     Color::Modifier def(Color::FG_DEFAULT);
 
-    // Start the time loop
     while (time < finalTime)
     {
         time = time + dt;
@@ -314,13 +282,14 @@ void reducedUnsteadyNS::solveOnline_sup(Eigen::MatrixXd& vel_now, label startSna
         }
         counter ++;
     }
-    // Save the solution
+    // Export the solution
     ITHACAstream::exportMatrix(online_solution, "red_coeff", "python", "./ITHACAoutput/red_coeff");
     ITHACAstream::exportMatrix(online_solution, "red_coeff", "matlab", "./ITHACAoutput/red_coeff");
     count_online_solve += 1;
 }
 
-// * * * * * * * * * * * * * * * Solve Functions  * * * * * * * * * * * * * //
+// * * * * * * * * * * * * * * * Solve Functions PPE * * * * * * * * * * * * * //
+
 void reducedUnsteadyNS::solveOnline_PPE(Eigen::MatrixXd& vel_now, label startSnap)
 {
     // Create and resize the solution vector
@@ -328,8 +297,10 @@ void reducedUnsteadyNS::solveOnline_PPE(Eigen::MatrixXd& vel_now, label startSna
     y.setZero();
 
     // Set Initial Conditions
-    y.head(Nphi_u) = ITHACAutilities::get_coeffs(Usnapshots[startSnap], Umodes);;
-    y.tail(Nphi_p) = ITHACAutilities::get_coeffs(Psnapshots[startSnap], Pmodes);;
+    // y.head(Nphi_u) = ITHACAutilities::get_coeffs(problem->Ufield[startSnap], Umodes);
+    // y.tail(Nphi_p) = ITHACAutilities::get_coeffs(problem->Pfield[startSnap], Pmodes);
+    y.head(Nphi_u) = ITHACAutilities::get_coeffs(Usnapshots[startSnap], Umodes);
+    y.tail(Nphi_p) = ITHACAutilities::get_coeffs(Psnapshots[startSnap], Pmodes);
 
     // Change initial condition for the lifting function
     for (label j = 0; j < N_BC; j++)
@@ -341,8 +312,8 @@ void reducedUnsteadyNS::solveOnline_PPE(Eigen::MatrixXd& vel_now, label startSna
     newton_object_PPE.nu = nu;
     newton_object_PPE.y_old = y;
     newton_object_PPE.dt = dt;
-    newton_object_PPE.B_matrix = B_matrix;
     newton_object_PPE.BC.resize(N_BC);
+
     for (label j = 0; j < N_BC; j++)
     {
         newton_object_PPE.BC(j) = vel_now(j, 0);
@@ -412,19 +383,17 @@ void reducedUnsteadyNS::solveOnline_PPE(Eigen::MatrixXd& vel_now, label startSna
         counter ++;
     }
 
-    // Save the solution
+    // Export the solution
     ITHACAstream::exportMatrix(online_solution, "red_coeff", "python", "./ITHACAoutput/red_coeff");
     ITHACAstream::exportMatrix(online_solution, "red_coeff", "matlab", "./ITHACAoutput/red_coeff");
     count_online_solve += 1;
 }
 
-void reducedUnsteadyNS::reconstruct_PPE(unsteadyNS& problem, fileName folder, int printevery)
+void reducedUnsteadyNS::reconstruct_PPE(fileName folder, int printevery)
 {
     mkDir(folder);
-    system("ln -s ../../constant " + folder + "/constant");
-    system("ln -s ../../0 " + folder + "/0");
-    system("ln -s ../../system " + folder + "/system");
-
+    ITHACAutilities::createSymLink(folder);
+    
     int counter = 0;
     int nextwrite = 0;
     int counter2 = 1;
@@ -438,28 +407,29 @@ void reducedUnsteadyNS::reconstruct_PPE(unsteadyNS& problem, fileName folder, in
             {
                 U_rec += Umodes[j] * online_solution[i](j + 1, 0);
             }
-            problem.exportSolution(U_rec,  name(counter2), folder);
+            problem->exportSolution(U_rec,  name(counter2), folder);
 
-            volScalarField P_rec("P_rec", Pmodes[0] * 0);
+            volScalarField P_rec("P_rec", problem->Pmodes[0] * 0);
             for (label j = 0; j < Nphi_p; j++)
             {
-                P_rec += Pmodes[j] * online_solution[i](j + Nphi_u + 1, 0);
+                P_rec += problem->Pmodes[j] * online_solution[i](j + Nphi_u + 1, 0);
             }
-            problem.exportSolution(P_rec, name(counter2), folder);
+            problem->exportSolution(P_rec, name(counter2), folder);
             nextwrite += printevery;
             counter2 ++;
+
+            UREC.append(U_rec);
+            PREC.append(P_rec);
         }
         counter++;
     }
 }
 
-void reducedUnsteadyNS::reconstruct_sup(unsteadyNS& problem, fileName folder, int printevery)
+void reducedUnsteadyNS::reconstruct_sup(fileName folder, int printevery)
 {
     mkDir(folder);
-    system("ln -s ../../constant " + folder + "/constant");
-    system("ln -s ../../0 " + folder + "/0");
-    system("ln -s ../../system " + folder + "/system");
-
+    ITHACAutilities::createSymLink(folder);
+    
     int counter = 0;
     int nextwrite = 0;
     int counter2 = 1;
@@ -473,21 +443,21 @@ void reducedUnsteadyNS::reconstruct_sup(unsteadyNS& problem, fileName folder, in
             {
                 U_rec += Umodes[j] * online_solution[i](j + 1, 0);
             }
-            //problem.exportSolution(U_rec, name(online_solution[i](0, 0)), folder);
-            problem.exportSolution(U_rec,  name(counter2), folder);
+            problem->exportSolution(U_rec,  name(counter2), folder);
 
-            volScalarField P_rec("P_rec", Pmodes[0] * 0);
+            volScalarField P_rec("P_rec", problem->Pmodes[0] * 0);
             for (label j = 0; j < Nphi_p; j++)
             {
-                P_rec += Pmodes[j] * online_solution[i](j + Nphi_u + 1, 0);
+                P_rec += problem->Pmodes[j] * online_solution[i](j + Nphi_u + 1, 0);
             }
-            //problem.exportSolution(P_rec, name(online_solution[i](0, 0)), folder);
-            problem.exportSolution(P_rec, name(counter2), folder);
+            problem->exportSolution(P_rec, name(counter2), folder);
             nextwrite += printevery;
             counter2 ++;
+
+            UREC.append(U_rec);
+            PREC.append(P_rec);
         }
         counter++;
     }
 }
 // ************************************************************************* //
-
