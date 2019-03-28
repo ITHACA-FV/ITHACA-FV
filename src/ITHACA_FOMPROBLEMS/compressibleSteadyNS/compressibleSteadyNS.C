47aa9271911ad0447c6be135fbc3acf5b6326748
Matteo Zancanaro <matteo.zancanaro@mail.polimi.it>
2019-03-28 14:57:54 +0100

    New branch to restart working on compressible flows

---
+++ src/ITHACA_FOMPROBLEMS/compressibleSteadyNS/compressibleSteadyNS.C
@@ -0,0 +1,122 @@
+/*---------------------------------------------------------------------------*\
+     ██╗████████╗██╗  ██╗ █████╗  ██████╗ █████╗       ███████╗██╗   ██╗
+     ██║╚══██╔══╝██║  ██║██╔══██╗██╔════╝██╔══██╗      ██╔════╝██║   ██║
+     ██║   ██║   ███████║███████║██║     ███████║█████╗█████╗  ██║   ██║
+     ██║   ██║   ██╔══██║██╔══██║██║     ██╔══██║╚════╝██╔══╝  ╚██╗ ██╔╝
+     ██║   ██║   ██║  ██║██║  ██║╚██████╗██║  ██║      ██║      ╚████╔╝
+     ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝      ╚═╝       ╚═══╝
+
+ * In real Time Highly Advanced Computational Applications for Finite Volumes
+ * Copyright (C) 2017 by the ITHACA-FV authors
+-------------------------------------------------------------------------------
+
+  License
+  This file is part of ITHACA-FV
+
+  ITHACA-FV is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  ITHACA-FV is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with ITHACA-FV. If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+
+/// \file
+/// Source file of the steadyNS class.
+
+#include "compressibleSteadyNS.H"
+#include "viscosityModel.H"
+
+// * * * * * * * * * * * * * * * Constructors * * * * * * * * * * * * * * * * //
+
+// Constructor
+compressibleSteadyNS::compressibleSteadyNS() {}
+compressibleSteadyNS::compressibleSteadyNS(int argc, char* argv[])
+{
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    _simple = autoPtr<simpleControl>
+              (
+                  new simpleControl
+                  (
+                      mesh
+                  )
+              );
+    simpleControl& simple = _simple();
+#include "createFields.H"
+//#include "createFvOptions.H"
+    supex = ITHACAutilities::check_sup();
+    turbulence->validate();
+    ITHACAdict = new IOdictionary
+    (
+        IOobject
+        (
+            "ITHACAdict",
+            runTime.system(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        )
+    );
+    tolerance = ITHACAdict->lookupOrDefault<scalar>("tolerance", 1e-5);
+    maxIter = ITHACAdict->lookupOrDefault<scalar>("maxIter", 1000);
+    para = new ITHACAparameters;
+}
+
+
+// * * * * * * * * * * * * * * Full Order Methods * * * * * * * * * * * * * * //
+
+// Method to perform a truthSolve
+void compressibleSteadyNS::truthSolve(List<scalar> mu_now)
+{
+    Time& runTime = _runTime();
+    fvMesh& mesh = _mesh();
+    volScalarField& E = _E();
+    volScalarField& rho = _rho();
+    volScalarField& psi = _psi();    
+    volVectorField& U = _U();
+    pressureControl& pressureControl=_pressureControl(); 
+    dimensionedScalar& initialMass = _initialMass();
+    surfaceScalarField& phi = _phi();
+    fv::options& fvOptions = _fvOptions();
+    simpleControl& simple = _simple();
+    IOMRFZoneList& MRF = _MRF();
+    fluidThermo& thermo = pThermo();
+    volScalarField& p = pThermo().p();
+    Info << thermo.mu() << endl;
+    Info << thermo.nu() << endl;
+#include "NLsolve.H"
+    ITHACAstream::exportSolution(U, name(counter), "./ITHACAoutput/Offline/");
+    ITHACAstream::exportSolution(p, name(counter), "./ITHACAoutput/Offline/");
+    ITHACAstream::exportSolution(E, name(counter), "./ITHACAoutput/Offline/");  
+    volScalarField _nut(turbulence->nut());
+    ITHACAstream::exportSolution(_nut, name(counter), "./ITHACAoutput/Offline/");
+    Ufield.append(U);
+    Pfield.append(p);
+    Efield.append(E);
+    nutFields.append(_nut);
+    counter++;
+    writeMu(mu_now);
+    mu_samples.conservativeResize(mu_samples.rows() + 1, mu_now.size());
+}
+
+void compressibleSteadyNS::change_viscosity(double mu_new)
+{
+    const volScalarField& mu =  pThermo().mu();
+    volScalarField& mu_field = const_cast<volScalarField&>(mu);
+    this->assignIF(mu_field, mu_new);
+
+    for (int i = 0; i < mu_field.boundaryFieldRef().size(); i++)
+    {
+        this->assignBC(mu_field, i, mu_new);
+    }
+}
