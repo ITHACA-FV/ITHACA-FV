da82f848307281918870a11238d88482b7d2981e
giovastabile <giovanni.stabile87@gmail.com>
2019-06-05 14:31:48 +0200

    FIX: proper indentation of the files

--- src/ITHACA_CORE/ITHACAutilities/ITHACAutilities.C
+++ src/ITHACA_CORE/ITHACAutilities/ITHACAutilities.C
@@ -683,7 +683,10 @@ double ITHACAutilities::H1seminorm(volVectorField field)
 void ITHACAutilities::setBoxToValue(volScalarField& field, Eigen::MatrixXd Box,
                                     double value)
 {
-    M_Assert(Box.rows() == 2 && Box.cols() == 3, "The box must be a 2*3 matrix shaped in this way: \nBox = \t|x0, y0, z0|\n\t|x1, yi, z1|\n");
+    M_Assert(Box.rows() == 2
+             && Box.cols() == 3,
+             "The box must be a 2*3 matrix shaped in this way: \nBox = \t|x0, y0, z0|\n\t|x1, yi, z1|\n");
+
     for (label i = 0; i < field.internalField().size(); i++)
     {
         auto cx = field.mesh().C()[i].component(vector::X);
@@ -1266,9 +1269,12 @@ void ITHACAutilities::assignMixedBC(
 }
 
 template<typename type_f>
-List<int> ITHACAutilities::getIndicesFromBox(GeometricField<type_f, fvPatchField, volMesh>& field, Eigen::MatrixXd Box)
+List<int> ITHACAutilities::getIndicesFromBox(
+    GeometricField<type_f, fvPatchField, volMesh>& field, Eigen::MatrixXd Box)
 {
-    M_Assert(Box.rows() == 2 && Box.cols() == 3, "The box must be a 2*3 matrix shaped in this way: \nBox = \t|x0, y0, z0|\n\t|x1, yi, z1|\n");
+    M_Assert(Box.rows() == 2
+             && Box.cols() == 3,
+             "The box must be a 2*3 matrix shaped in this way: \nBox = \t|x0, y0, z0|\n\t|x1, yi, z1|\n");
     List<int> indices(field.internalField().size());
     int k = 0;
 
@@ -1277,6 +1283,7 @@ List<int> ITHACAutilities::getIndicesFromBox(GeometricField<type_f, fvPatchField
         auto cx = field.mesh().C()[i][0];
         auto cy = field.mesh().C()[i][1];
         auto cz = field.mesh().C()[i][2];
+
         if (cx >= Box(0, 0) && cy >= Box(0, 1) && cz >= Box(0, 2) && cx <= Box(1, 0)
                 && cy <= Box(1, 1) && cz <= Box(1, 2) )
         {
@@ -1284,17 +1291,19 @@ List<int> ITHACAutilities::getIndicesFromBox(GeometricField<type_f, fvPatchField
             k++;
         }
     }
+
     indices.resize(k);
     return indices;
 }
 
 template<typename type_f>
-fvMeshSubset* ITHACAutilities::getSubMeshFromBox(GeometricField<type_f, fvPatchField, volMesh>& field, Eigen::MatrixXd Box)
+fvMeshSubset* ITHACAutilities::getSubMeshFromBox(
+    GeometricField<type_f, fvPatchField, volMesh>& field, Eigen::MatrixXd Box)
 {
     List<int> indices = getIndicesFromBox(field, Box);
     fvMeshSubset* sub;
     sub = new fvMeshSubset(field.mesh());
-    (field.mesh());    
+    (field.mesh());
 #if OPENFOAM >= 1812
     sub->setCellSubset(indices);
 #else
@@ -1303,11 +1312,15 @@ fvMeshSubset* ITHACAutilities::getSubMeshFromBox(GeometricField<type_f, fvPatchF
     return sub;
 }
 
-template fvMeshSubset* ITHACAutilities::getSubMeshFromBox(GeometricField<scalar, fvPatchField, volMesh>& field, Eigen::MatrixXd Box);
-template fvMeshSubset* ITHACAutilities::getSubMeshFromBox(GeometricField<vector, fvPatchField, volMesh>& field, Eigen::MatrixXd Box);
+template fvMeshSubset* ITHACAutilities::getSubMeshFromBox(
+    GeometricField<scalar, fvPatchField, volMesh>& field, Eigen::MatrixXd Box);
+template fvMeshSubset* ITHACAutilities::getSubMeshFromBox(
+    GeometricField<vector, fvPatchField, volMesh>& field, Eigen::MatrixXd Box);
 
-template List<int> ITHACAutilities::getIndicesFromBox(GeometricField<scalar, fvPatchField, volMesh>& field, Eigen::MatrixXd Box);
-template List<int> ITHACAutilities::getIndicesFromBox(GeometricField<vector, fvPatchField, volMesh>& field, Eigen::MatrixXd Box);
+template List<int> ITHACAutilities::getIndicesFromBox(
+    GeometricField<scalar, fvPatchField, volMesh>& field, Eigen::MatrixXd Box);
+template List<int> ITHACAutilities::getIndicesFromBox(
+    GeometricField<vector, fvPatchField, volMesh>& field, Eigen::MatrixXd Box);
 
 template PtrList<volScalarField> ITHACAutilities::reconstruct_from_coeff(
     PtrList<volScalarField>& modes, Eigen::MatrixXd& coeff_matrix, label Nmodes);
