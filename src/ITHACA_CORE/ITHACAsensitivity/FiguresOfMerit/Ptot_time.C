4bb2a5064fd7246eb48035eb5ff883574908045a
giovastabile <giovanni.stabile87@gmail.com>
2019-08-02 11:33:40 +0200

    Formatted Code

--- src/ITHACA_CORE/ITHACAsensitivity/FiguresOfMerit/Ptot_time.C
+++ src/ITHACA_CORE/ITHACAsensitivity/FiguresOfMerit/Ptot_time.C
@@ -2,46 +2,50 @@
 
 Ptot_time::Ptot_time() {}
 
-Ptot_time::Ptot_time(int argc, char *argv[], int Nsampled)
+Ptot_time::Ptot_time(int argc, char* argv[], int Nsampled)
 {
-	Npoints=Nsampled;
-	modelOutput.resize(Npoints);
-	modelOutput.setZero();	
-	name={"Ptot_time"};	
-	#include "setRootCase.H"
-	#include "createTime.H"
-	#include "createMesh.H"	
-	#include "createpowerDens.H"
+    Npoints = Nsampled;
+    modelOutput.resize(Npoints);
+    modelOutput.setZero();
+    name = {"Ptot_time"};
+#include "setRootCase.H"
+#include "createTime.H"
+#include "createMesh.H"
+#include "createpowerDens.H"
 }
 
 void Ptot_time::buildMO(std::string dir, int t)
 {
-	Time& runTime = _runTime();
-	fvMesh& mesh = _mesh();
-	volScalarField& powerDens=_powerDens();
-	std::string folder=dir;
-	if (ITHACAutilities::check_folder(folder)==true)
-	{
-		for (int j=0; j<Npoints; j++)
-		{
-			folder.append(std::to_string(j));
-			folder.append("/");
-			ITHACAstream::read_fields(ptrfield,powerDens,folder); 
-			for (int i=0; i<ptrfield.size(); i++)
-			{
-				if (i==t){
+    Time& runTime = _runTime();
+    fvMesh& mesh = _mesh();
+    volScalarField& powerDens = _powerDens();
+    std::string folder = dir;
 
-modelOutput(j)=fvc::domainIntegrate(ptrfield[i]).value();}
-			}
-			folder=dir; 
-			ptrfield.clear();
-		}
-		MObuilt=true;
-	} 
-	else
-	{
-		std::cout<<"Outputs of the model are not computed yet, programm aborted"<<std::endl;
-				
-	}
+    if (ITHACAutilities::check_folder(folder) == true)
+    {
+        for (int j = 0; j < Npoints; j++)
+        {
+            folder.append(std::to_string(j));
+            folder.append("/");
+            ITHACAstream::read_fields(ptrfield, powerDens, folder);
 
+            for (int i = 0; i < ptrfield.size(); i++)
+            {
+                if (i == t)
+                {
+                    modelOutput(j) = fvc::domainIntegrate(ptrfield[i]).value();
+                }
+            }
+
+            folder = dir;
+            ptrfield.clear();
+        }
+
+        MObuilt = true;
+    }
+    else
+    {
+        std::cout << "Outputs of the model are not computed yet, programm aborted" <<
+                  std::endl;
+    }
 }
