226133b9a7a6194b2786f810a821e16584296b99
giovastabile <giovanni.stabile87@gmail.com>
2019-12-11 14:08:19 +0100

    Added tutorial for multiphase flows

---
+++ tutorials/13InterFoam/13interFoam.C
@@ -0,0 +1,61 @@
+/*---------------------------------------------------------------------------*\
+     ██╗████████╗██╗  ██╗ █████╗  ██████╗ █████╗       ███████╗██╗   ██╗
+     ██║╚══██╔══╝██║  ██║██╔══██╗██╔════╝██╔══██╗      ██╔════╝██║   ██║
+     ██║   ██║   ███████║███████║██║     ███████║█████╗█████╗  ██║   ██║
+     ██║   ██║   ██╔══██║██╔══██║██║     ██╔══██║╚════╝██╔══╝  ╚██╗ ██╔╝
+     ██║   ██║   ██║  ██║██║  ██║╚██████╗██║  ██║      ██║      ╚████╔╝
+     ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝      ╚═╝       ╚═══╝
+
+ * In real Time Highly Advanced Computational Applications for Finite Volumes
+ * Copyright (C) 2017 by the ITHACA-FV authors
+-------------------------------------------------------------------------------
+License
+    This file is part of ITHACA-FV
+    ITHACA-FV is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    ITHACA-FV is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with ITHACA-FV. If not, see <http://www.gnu.org/licenses/>.
+Description
+    Example of steady NS Reduction Problem
+SourceFiles
+    03steadyNS.C
+\*---------------------------------------------------------------------------*/
+
+#include "unsteadyNSmulti.H"
+#include "ITHACAstream.H"
+#include "ITHACAPOD.H"
+#include "reducedSimpleSteadyNS.H"
+#include "forces.H"
+#include "IOmanip.H"
+
+
+class tutorial13 : public unsteadyNSmulti
+{
+    public:
+        /// Constructor
+        explicit tutorial13(int argc, char* argv[])
+            :
+            unsteadyNSmulti(argc, argv)
+        {}
+
+};
+
+int main(int argc, char* argv[])
+{
+    // Construct the tutorial object
+    List<scalar> ciao;
+    tutorial13 example(argc, argv);
+    example.startTime = 0;
+    example.finalTime = 20;
+    example.timeStep = 0.01;
+    example.writeEvery = 1.0;
+
+    example.truthSolve(ciao);
+    
+}
