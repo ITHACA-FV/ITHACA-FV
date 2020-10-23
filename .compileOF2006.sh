59d5cb3731cc56f66af4f82650abaa8b35f49710
umbertoemil <umbertoemil.morelli@usc.es>
2020-10-23 11:45:11 +0200

    TEST: Modify the github actions files to compile also the dependeces on MUQ2 and PYTORCH

--- .compileOF2006.sh
+++ .compileOF2006.sh
@@ -1,4 +1,4 @@
-docker pull openfoamplus/of_v2006_centos73
-docker run -ti -d --name foam2006 -v ${PWD}:/home/ofuser/app:rw openfoamplus/of_v2006_centos73 /bin/sh
-docker exec foam2006 /bin/sh -c "cd /home/ofuser/app; ls"
-docker exec foam2006 /bin/sh -c "source /opt/OpenFOAM/setImage_v2006.sh; cd /home/ofuser/app; source etc/bashrc; ./Allwmake -tau"
+docker pull ithacafv/openfoam2006-muq2-pytorch
+docker run -ti -d --name foam2006 -v "${PWD}":/home/ofuser/app:rw ithacafv/openfoam2006-muq2-pytorch /bin/bash
+docker exec foam2006 /bin/bash -c "cd /home/ofuser/app; ls"
+docker exec foam2006 /bin/bash -c "export MUQ_LIBRARIES=/home/Installations/MUQ_INSTALL; export MUQ_EXT_LIBRARIES=/home/Installations/MUQ_INSTALL/muq_external; export TORCH_LIBRARIES=/pytorch/torch; cd /home/ofuser/app; source /usr/lib/openfoam/openfoam2006/etc/bashrc; source etc/bashrc; ./Allwmake -taumq"
