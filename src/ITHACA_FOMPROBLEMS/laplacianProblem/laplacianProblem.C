8835f435a0fb74ea135f27a21c08605b7a59a87d
giovastabile <giovanni.stabile87@gmail.com>
2021-08-13 15:34:35 +0200

    added pybind example

--- src/ITHACA_FOMPROBLEMS/laplacianProblem/laplacianProblem.C
+++ src/ITHACA_FOMPROBLEMS/laplacianProblem/laplacianProblem.C
@@ -122,6 +122,11 @@ void laplacianProblem::project(label Nmodes)
                                           nu_list[i], Tmodes[k])).value();
             }
         }
+
+        if (Pstream::parRun())
+        {
+            reduce(A_matrices[i], sumOp<Eigen::MatrixXd>());
+        }
     }
 
     /// Export the A matrices
