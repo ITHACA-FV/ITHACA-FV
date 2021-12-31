af3d243534b33d3492e211f018f1021a8c0a986d
giovastabile <giovanni.stabile87@gmail.com>
2021-12-31 12:07:09 +0100

    FEAT: adding skeleton for Burgers FOM class with minimal tutorial

---
+++ tutorials/CFD/23burgers/23burgers.C
@@ -0,0 +1,65 @@
+/*---------------------------------------------------------------------------*\
+     ██╗████████╗██╗  ██╗ █████╗  ██████╗ █████╗       ███████╗██╗   ██╗
+     ██║╚══██╔══╝██║  ██║██╔══██╗██╔════╝██╔══██╗      ██╔════╝██║   ██║
+     ██║   ██║   ███████║███████║██║     ███████║█████╗█████╗  ██║   ██║
+     ██║   ██║   ██╔══██║██╔══██║██║     ██╔══██║╚════╝██╔══╝  ╚██╗ ██╔╝
+     ██║   ██║   ██║  ██║██║  ██║╚██████╗██║  ██║      ██║      ╚████╔╝
+     ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝      ╚═╝       ╚═══╝
+
+ * In real Time Highly Advanced Computational Applications for Finite Volumes
+ * Copyright (C) 2017 by the ITHACA-FV authors
+-------------------------------------------------------------------------------
+License
+    This file is part of ITHACA-FV
+    ITHACA-FV is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    ITHACA-FV is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with ITHACA-FV. If not, see <http://www.gnu.org/licenses/>.
+Description
+    Example of a heat transfer Reduction Problem
+SourceFiles
+    02thermalBlock.C
+\*---------------------------------------------------------------------------*/
+
+#include <iostream>
+#include "fvCFD.H"
+#include "IOmanip.H"
+#include "Time.H"
+#include "Burgers.H"
+#include <Eigen/Dense>
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+/// \brief Class where the tutorial number 2 is implemented.
+/// \details It is a child of the laplacianProblem class and some of its
+/// functions are overridden to be adapted to the specific case.
+class tutorial23: public Burgers
+{
+    public:
+        explicit tutorial23(int argc, char* argv[])
+            :
+            Burgers(argc, argv),
+            U(_U())
+        {}
+
+        /// Velocity field
+        volVectorField& U;
+};
+
+
+int main(int argc, char* argv[])
+{
+    // Create the train object of the tutorial02 type
+    tutorial23 train(argc, argv);
+    // Read some parameters from file
+    ITHACAparameters* para = ITHACAparameters::getInstance(train._mesh(),
+                             train._runTime());
+
+    train.truthSolve();
+}
