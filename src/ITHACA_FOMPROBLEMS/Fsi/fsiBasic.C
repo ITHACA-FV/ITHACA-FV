f4aef307e3fd5f14f4623f9f7fac896d9f764d31
valetinnkana <valentin.nkana@intermaths.eu>
2022-03-21 01:18:10 +0100

    testing the effect of the method init on the mesh

--- src/ITHACA_FOMPROBLEMS/Fsi/fsiBasic.C
+++ src/ITHACA_FOMPROBLEMS/Fsi/fsiBasic.C
@@ -389,12 +389,12 @@ void fsiBasic::restart()
     //runTime.setTime(0, 1);
     instantList Times = runTime.times();
     runTime.setTime(Times[1], 1);
-    //meshPtr.clear();
+    mesh.clear();
     //meshPtr().resetMotion();
     _pimple.clear();
     //meshPtr().movePoints(point0);
     //meshPtr = autoPtr<Foam::dynamicFvMesh> (dynamicFvMesh::New(args, runTime));
-    meshPtr().init(false);
+    //meshPtr().init(false);
     Foam::dynamicFvMesh& mesh = meshPtr();
     _pimple = autoPtr<pimpleControl>
                    (
