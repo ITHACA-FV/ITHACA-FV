72cc483a87a3cffd94dbf3729f0bb61a4e8e4522
Pavan Pranjivan Mehta <57907893+mehta-pavan@users.noreply.github.com>
2023-01-16 21:37:34 +0100

    Update Of.sh

--- singularity/Of.sh
+++ singularity/Of.sh
@@ -1,7 +1,7 @@
-#sourcing the correct bashrc is important
-#bashrc for OpenFOAM 
+##sourcing the correct bashrc is important
+##bashrc for OpenFOAM 
 source /usr/lib/openfoam/openfoam2106/etc/bashrc
 
-./Allrun
-#icoFoam #-paralell
-#perform_POD
+##./Allrun
+icoFoam ##-paralell
+perform_POD
