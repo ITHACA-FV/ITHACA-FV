bdf81df795e7f23a2d1db8cc6dbaa75a08cebbc0
Shenhui,Ruan <shenhui.ruan@kit.edu>
2025-07-12 22:20:59 +0200

    FIX:fatal mount of .compileOF2406 due to root

--- .compileOF2406.sh
+++ .compileOF2406.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 docker pull shenhuiruan/ithaca2406
 docker run -ti -d --name foam2406 -v "${PWD}":/home/foam:rw shenhuiruan/ithaca2406 /bin/bash
-docker exec foam2406 /bin/bash -c "source /root/OpenFOAM/OpenFOAM-v2406/etc/bashrc; cd /home/foam; source etc/bashrc; git submodule update --init; ./Allwmake -taumq"
+docker exec foam2406 /bin/bash -c "
+  git config --global --add safe.directory /home/foam && \
+  source /root/OpenFOAM/OpenFOAM-v2406/etc/bashrc && \
+  cd /home/foam && \
+  source etc/bashrc && \
+  git submodule update --init && \
+  ./Allwmake -taumq
+"
