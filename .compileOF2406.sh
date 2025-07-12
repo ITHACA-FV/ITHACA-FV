481b24809532adb40dd506319404cdc4b0992276
Shenhui,Ruan <shenhui.ruan@kit.edu>
2025-07-12 22:02:08 +0200

    FIX:.compileOF2406.sh

--- .compileOF2406.sh
+++ .compileOF2406.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker pull shenhuiruan/ithaca2406
-docker run -ti -d --name foam2406 -v "${PWD}":/root:rw shenhuiruan/ithaca2406 /bin/bash
-docker exec foam2406 /bin/bash -c "source /root/OpenFOAM/OpenFOAM-v2406/etc/bashrc; cd /root; source etc/bashrc; git submodule update --init; ./Allwmake -taumq"
+docker run -ti -d --name foam2406 -v "${PWD}":/home:rw shenhuiruan/ithaca2406 /bin/bash
+docker exec foam2406 /bin/bash -c "source /root/OpenFOAM/OpenFOAM-v2406/etc/bashrc; cd /home; source etc/bashrc; git submodule update --init; ./Allwmake -taumq"
